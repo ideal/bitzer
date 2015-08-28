bd1808ee19c06793bd26618b302f7613dc9f081f
shangyuanchun <yuanchun.syc@alibaba-inc.com>
2015-08-28 12:09:36 +0800

    need to change the name

--- src/main.c
+++ src/main.c
@@ -7,13 +7,13 @@
  * Foundation; either version 3 of the License, or (at your option)
  * any later version.
  *
- * bitzer is distributed in the hope that it will be useful,
+ * Bitzer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with bitzer. If not, write to:
+ * along with Bitzer. If not, write to:
  * The Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor
  * Boston, MA 02110-1301, USA.
